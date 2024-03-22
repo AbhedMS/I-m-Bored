382a71a4adfc9e08a0b8029009106944a94f44d4
Abhed M S <abhedmesct@gmail.com>
2024-03-22 23:24:02 +0530

    Able to successfully render a public API

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://bored-api.appbrewery.com/random");
+    const result = response.data;
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
+
+app.post("/", async (req, res) => {
+  console.log(req.body);
+  try {
+    var url = "https://bored-api.appbrewery.com/filter?type=" + req.body.type + "&participants=" + req.body.participants;
+    console.log(url);
+    const response = await axios.get(url);
+    const result = response.data;
+    var randomNum = Math.floor(Math.random() * result.length);
+    res.render("index.ejs", { data: result[randomNum]});
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message, type: req.body.type, participants: req.body.participants
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
